80f67bc0dce33f5fef27402a4b58ee9336a367c6
Light <127822286+Light-of-dark@users.noreply.github.com>
2024-06-23 13:13:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ function handleBirthdayResponse(isBirthday) {
     const birthday = new Date(today.getFullYear(), 6, 6); // July 6
     const responseElement = document.getElementById('birthdayResponse');
     const isTodayBirthday = today.getMonth() === birthday.getMonth() && today.getDate() === birthday.getDate();
-    const bday = document.getElementById('cameraSound');
+    const bday = document.getElementById('bday');
     bday.play();
-    
+    playCameraSound()
     if (isBirthday) {
         if (isTodayBirthday) {
             responseElement.textContent = "Happy bday Amal!";
